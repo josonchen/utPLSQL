8d349a9503659c5311d6ccab559a410d86399b5d
Jacek Gebal <jgebal@users.noreply.github.com>
2018-09-09 23:11:00 +0100

    Adding timing to install process to see where time goes.

--- .travis/install.sh
+++ .travis/install.sh
@@ -10,6 +10,9 @@ set verify off
 
 alter session set plsql_warnings = 'ENABLE:ALL', 'DISABLE:(5004,5018,6000,6001,6003,6009,6010,7206)';
 alter session set plsql_optimize_level=0;
+
+set timing on
+set time on
 @install_headless.sql $UT3_OWNER $UT3_OWNER_PASSWORD
 SQL
 
@@ -54,6 +57,8 @@ fi
 #additional privileges to run scripted tests
 time "$SQLCLI" sys/$ORACLE_PWD@//$CONNECTION_STR AS SYSDBA <<-SQL
 set feedback on
+set timing on
+set time on
 --needed for Mystats script to work
 grant select any dictionary to $UT3_OWNER;
 --Needed for testing a coverage outside ut3_owner.
@@ -63,6 +68,8 @@ SQL
 #Create user that will own the tests
 time "$SQLCLI" sys/$ORACLE_PWD@//$CONNECTION_STR AS SYSDBA <<-SQL
 set feedback off
+set timing on
+set time on
 @create_utplsql_owner.sql $UT3_TESTER $UT3_TESTER_PASSWORD $UT3_TABLESPACE
 
 --needed for testing distributed transactions
@@ -79,6 +86,8 @@ SQL
 #Create additional UT3$USER# to test for special characters
 time "$SQLCLI" sys/$ORACLE_PWD@//$CONNECTION_STR AS SYSDBA <<-SQL
 set feedback off
+set timing on
+set time on
 @create_utplsql_owner.sql $UT3_USER $UT3_USER_PASSWORD $UT3_TABLESPACE
 --Grant UT3 framework to UT3$USER#
 @create_user_grants.sql $UT3_OWNER $UT3_USER
--- source/install_component.sql
+++ source/install_component.sql
@@ -17,7 +17,9 @@
 set heading off
 set feedback off
 exec dbms_output.put_line('Installing component '||upper(regexp_substr('&&1','\/(\w*)\.',1,1,'i',1)));
+set timing on
 @@&&1
 show errors
+set timing off
 exec dbms_output.put_line('&&line_separator');
 
